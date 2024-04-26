166da430397121e957c58890d4fbc0793f72b9c7
dezeekees <tijnpeeters2004@gmail.com>
2024-04-26 19:19:13 +0200

    gamescope

--- system/hyprland.nix
+++ system/hyprland.nix
@@ -7,6 +7,10 @@
     xwayland.enable = true;
   };
 
+  programs.gamescope = {
+    enable = true;
+  };
+
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     WLR_NO_HARDWARE_CURSORS = "1";
