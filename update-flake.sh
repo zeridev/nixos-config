64257ebe58ae3c02ae07280d90d97ba09b4f1d0e
DeZeeKees <tijnpeeters2004@gmail.com>
2025-10-08 18:41:22 +0200

    fix update script

--- update-flake.sh
+++ update-flake.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+# Clear old generations
+sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations +5
+
+# Clean old store paths
+sudo nix-collect-garbage
+
 # Update flake inputs
 nix flake update
 
 # Rebuild NixOS
 sudo nixos-rebuild switch --flake ~/flake
-
-# Remove old generations older than 14 days
-sudo nix-collect-garbage --delete-older-than 14d
