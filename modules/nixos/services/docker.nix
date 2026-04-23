fec4d9a0e258250c9f90cadd0bbd3a0cb0bb3bee
DeZeeKees <tijnpeeters2004@gmail.com>
2026-04-23 20:41:00 +0200

    musicbrainz picard

--- modules/nixos/services/docker.nix
+++ modules/nixos/services/docker.nix
@@ -11,15 +11,38 @@ in
       default = "/mnt/other1/docker-data";
     };
 
-    
+
   };
 
   config = lib.mkIf cfg.enable {
-    virtualisation.docker.enable = true;
     users.extraGroups.docker.members = [ "dezeekees" ];
 
-    virtualisation.docker.daemon.settings = {
-      data-root = cfg.data-root;
+    virtualisation = {
+      docker = {
+        enable = true;
+        daemon.settings = {
+          data-root = cfg.data-root;
+        };
+      };
+
+      oci-containers.containers = {
+
+        picard = {
+          image = "aandree5/picard-web:latest";
+          ports = [ "5000:5000" ];
+          autoStart = true;
+          environment = {
+            PUID = "989";
+            PGID = "986";
+          };
+          volumes =[
+            "/mnt/other1/picard-web:/picard-web:rw"
+            "/mnt/other1/music:/music:rw"
+            "/mnt/other1/soulseek:/soulseek:rw"
+          ];
+        };
+
+      };
     };
   };
 }
